4ac81fc3a8ab762efabbc680f50f57fabab72cc9
“@kayolafarms” <joseph.olagabriel@gmail.com>
2022-12-05 06:18:14 +0100

    first commit

---
+++ Scripts/VM_Setup_multivm/linuxsysinfos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+### This script prints system info
+
+Echo “Welcome to bash script.”
+Echo
+Echo “#######################”
+
+Echo “The uptime of the system is:
+Uptime
+Echo
+Echo “#######################”
+
+Echo “Memory Utilization”
+Free –m
+Echo
+Echo “#######################”
+
+Echo “Disk Utilization”
+Df -h
---
+++ Scripts/VM_Setup_multivm/websitesetups_variable_script.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Variable Declaration
+PACKAGE="httpd wget unzip"
+SVC="httpd"
+URL="https://www.tooplate.com/zip-templates/2098_health.zip"
+ART_NAME="2098_health"
+TEMPDIR="/tmp/webfiles"
+
+
+
+# Installing Dependencies
+echo “###########################”
+echo “Installing Packages”
+echo “###########################”
+sudo yum install $PACKAGE –y
+echo
+
+# Start & Enable Service
+echo “###########################”
+echo “Start & Enable HTTPD Service”
+echo “###########################”
+sudo systemctl start $SVC
+sudo systemctl enable $SVC
+echo
+
+# Creating Temp Directory
+echo “###########################”
+echo “Starting Artifact Deployment”
+echo “###########################”
+mkdir –p $TEMPDIR
+cd $TEMPDIR
+echo
+wget $URL
+unzip $ART_NAME.zip
+sudo cp –r $ART_NAME/* /var/www/html/
+
+# Bounce Service
+echo “###########################”
+echo “Restarting HTTPD service”
+echo “###########################”
+systemctl restart $SVC
+echo 
+
+# Clean Up
+echo “###########################”
+echo “Removing Temporary Files”
+echo “###########################”
+rm –rf $TEMPDIR
+echo
+
